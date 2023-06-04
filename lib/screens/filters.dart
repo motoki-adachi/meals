7bb77b3666937eded5c83cec9f24b121ff6c1d0d
motoki-adachi <59963342+motoki-adachi@users.noreply.github.com>
2023-06-04 16:19:18 +0900

    168

---
+++ lib/screens/filters.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+
+class FilterScreen extends StatefulWidget {
+  const FilterScreen({super.key});
+
+  @override
+  State<FilterScreen> createState() {
+    return _FilterScreenState();
+  }
+}
+
+class _FilterScreenState extends State<FilterScreen> {
+  var _gutenFreeFilterSet = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Your Filters"),
+      ),
+      body: Column(
+        children: [
+          SwitchListTile(
+            value: _gutenFreeFilterSet,
+            onChanged: (isChecked) {
+              setState(() {
+                _gutenFreeFilterSet = isChecked;
+              });
+            },
+            title: Text(
+              "Gluten-Free",
+              style: Theme.of(context).textTheme.titleLarge!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            subtitle: Text(
+              "Only include gluten-free meals.",
+              style: Theme.of(context).textTheme.labelMedium!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                  ),
+            ),
+            activeColor: Theme.of(context).colorScheme.tertiary,
+            contentPadding: const EdgeInsets.only(left: 34, right: 22),
+          )
+        ],
+      ),
+    );
+  }
+}
